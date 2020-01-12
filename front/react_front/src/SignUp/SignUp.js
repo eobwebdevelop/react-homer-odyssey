b9e0516626d75ced206047005df18744e5d4160e
elenaOrtega <elenabaura56@gmail.com>
2020-01-12 21:32:32 +0000

    quest020

--- front/react_front/src/SignUp/SignUp.js
+++ front/react_front/src/SignUp/SignUp.js
@@ -4,24 +4,42 @@ class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: 'email@example.com'
+      email: "mon@email.com",
+      password: "monPassw0rd",
+      name: "James",
+      lastname: "Bond"
     }
   } 
 
   
   updateEmailField = (event) => {
-    this.setState({email:event.target.value})
-       
+    this.setState({email:event.target.value}) 
   }
+
+  handlerSubmit = (e) => {
+    e.preventDefault();
+    console.log("the form has been submited with these fields:", this.state );
+  }
+
 
   render() {
     return(
     <div> 
-        <from>
-          <h1>{this.state.email}</h1>
-          <label for="email">Enter your email:</label>
-          <input type="email" id="email" onChange = {this.updateEmailField} />
-        </from>
+        <form onSubmit={this.handlerSubmit}>
+          <h1>{JSON.stringify(this.state,1,1)}</h1>
+          <input type="text" id="name" placeholder="name" />
+          <br/>
+          <input type="text" id="lastname" placeholder="lastname" />
+          <br/>
+          <input type="email" placeholder="email" id="email" onChange = {this.updateEmailField} />
+          <br/>
+          <input type="password" id="password" placeholder="password" />
+          <br/>
+          <input type="password" id="passwordConf" placeholder="password confirmation" />
+          <br/> <br/>
+          <input type="submit" value="Submit"/> 
+
+        </form>
     </div>
     );
   }
