51c89050890c236469f0d85a832ac086c12883e8
elenaOrtega <elenabaura56@gmail.com>
2020-01-20 10:35:17 +0000

    materialDesign

--- front/react_front/src/App.js
+++ front/react_front/src/App.js
@@ -2,19 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './SignUp/SignUp';
+import {MuiThemeProvider, Grid, Paper} from '@material-ui/core/';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MuiThemeProvider  >
+          <Grid  container
+          alignItems='center'
+          style={{ height:  '100%' }}>
+                  <Grid  item  xs={12}>
 
-        <SignUp />
-      </header>
+                      <Paper
+                      elevation={4}
+                      style={{ margin:  32 }}
+                      >
+                              <Grid  container
+                              alignItems='center'
+                              justify='center'>
+                                   <Grid  item  xs={12}  sm={6} style={{ 'text-align':  'center' }}>
+                                        <img  src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png"  />
+                                    </Grid>
+                                  <Grid  item  xs={12} sm={6}
+                                  alignContent='center'
+                                  >
+                                          <SignUp  />
+                                  </Grid>
+                              </Grid>
+                      </Paper>
+                  </Grid>
+          </Grid>
+         </MuiThemeProvider>
     </div>
   );
 }
